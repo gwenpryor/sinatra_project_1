a504fedab2d5908445bbe64ae8e173ecfde94a09
Gwendolyn Pryor <Gwen.m.pryor@gmail.com>
2013-07-30 18:20:13 +0100

    Added Gemfile.lock

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
 
 get '/' do
+   "Curses! I've Forgotten a Curse!"
+   erb :index
+end
+
+get '/Fuck' do
+    "Avada Kedavra"
+    erb :index
+end
+get '/Shit' do
+    "Avis Oppugno"
+    erb :index
+end
+get '/Bloody Hell' do
+    "Confringo"
+    erb :index
+end
+get '/Slut' do
+    "Confundo"
     erb :index
 end
